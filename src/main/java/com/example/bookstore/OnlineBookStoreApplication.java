20dd9a68f1cf6a4a2db6bb244198af5b6380f6de
Borovski Cergei <borovskicerg0001@gmaol.com>
2025-01-24 18:28:53 +0200

    start homework

--- src/main/java/com/example/bookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/bookstore/OnlineBookStoreApplication.java
@@ -10,3 +10,9 @@ public class OnlineBookStoreApplication {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
 }
+/*
+GET: /api/books (Retrieve book catalog) (should be done in the previous PR)
+GET: /api/books/{id} (Retrieve book details) (should be done in the previous PR)
+POST: /api/books (Create a new book) (should be done in the previous PR)
+PUT: /api/books/{id} (Update a specific book)
+ */
